1bbca61b9be1d5840e32dda05cf13842d5f80262
Artem Karpenko <dante120591@gmail.com>
2023-08-02 13:36:16 +0300

    Basic routing done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { Layout } from './Layout';
-import { General } from './General';
-import { Create } from './Create';
+import { Layout } from './Layout/Layout';
+import GeneralPage from '../pages/GeneralPage';
+import CreatePage from '../pages/CreatePage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<General />} />
-        <Route path="/create" element={<Create />} />
+        <Route index element={<GeneralPage />} />
+        <Route path="/create" element={<CreatePage />} />
       </Route>
       <Route path="*" element={<Layout />} />
     </Routes>
---
+++ src/pages/CreatePage.js
@@ -0,0 +1,11 @@
+import { Create } from '../components/Create/Create';
+
+const CreatePage = () => {
+  return (
+    <div>
+      <Create />
+    </div>
+  );
+};
+
+export default CreatePage;
