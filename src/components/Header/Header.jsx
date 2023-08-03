ded79cd616072e55bc8ea94ea172bdef8a1a3925
Artem Karpenko <dante120591@gmail.com>
2023-08-03 21:01:38 +0300

    Header tablet done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,18 +6,19 @@ import css from './Header.module.css';
 export const Header = () => {
   return (
     <div className={css.container}>
-      <div className={css.upperHeader}>
-        <span className={css.title}>Event planner</span>
+      <div className={css.header}>
+        <span className={css.titleMobile}>Event planner</span>
         <div className={css.langSwitcher}>
           <span className={css.langTitle}>UK</span>
           <FaChevronDown />
         </div>
-      </div>
-      <div className={css.searchBar}>
-        <BiSearch
-          style={{ marginLeft: 12, width: 24, height: 24, fill: '#7B61FF' }}
-        />
-        <span className={css.searchTitle}>Search by keywords</span>
+        <div className={css.searchBar}>
+          <BiSearch
+            style={{ marginLeft: 12, width: 24, height: 24, fill: '#7B61FF' }}
+          />
+          <span className={css.searchTitle}>Search by keywords</span>
+        </div>
+        <span className={css.title}>Event planner</span>
       </div>
     </div>
   );
