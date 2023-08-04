08e1d2d53129795b02687dee4495b48958aa620b
Artem Karpenko <dante120591@gmail.com>
2023-08-04 19:15:51 +0300

    Create form on all devices done

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,32 +1,19 @@
-import { useEffect, useState } from 'react';
-
 import { BackButton } from 'components/BackButton/BackButton';
 import { FormNarrow } from './Form/FormNarrow';
 import { FormWide } from './Form/FormWide';
 
+import { useMatchMedia } from 'hooks/use-match-media';
+
 import css from './Create.module.css';
 
 export const Create = () => {
-  const [windowWidth, setWindowWidth] = useState('desktop');
+  const { isDesktop } = useMatchMedia();
 
-  useEffect(() => {
-    getWindowWidth();
-  }, []);
-
-  const getWindowWidth = () => {
-    const screenWidth = window.innerWidth;
-    console.log(screenWidth);
-    if (screenWidth <= 1280) {
-      setWindowWidth('mobile');
-    } else {
-      return;
-    }
-  };
   return (
     <div className={css.container}>
       <BackButton />
       <h1 className={css.pageTitle}>Create new event</h1>
-      {windowWidth === 'desktop' ? <FormWide /> : <FormNarrow />}
+      {isDesktop ? <FormWide /> : <FormNarrow />}
     </div>
   );
 };
