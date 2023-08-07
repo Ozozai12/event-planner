474521e4203a4be67e6c53c258d1786bf6669f6b
Artem Karpenko <dante120591@gmail.com>
2023-08-07 22:12:15 +0300

    ShowMore upgraded

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import priorityHandler from 'helpers/priorityHandler';
@@ -7,15 +6,9 @@ import dateFormatHandler from 'helpers/dateFormatHandler';
 import css from './EventCard.module.css';
 
 export const EventCard = ({ event }) => {
-  const [showMore, setShowMore] = useState(false);
-
   return (
-    <div
-      className={css.eventItem}
-      onMouseEnter={() => setShowMore(true)}
-      onMouseLeave={() => setShowMore(false)}
-    >
-      <div className={css.imageThumb} style={{ height: showMore ? 280 : 336 }}>
+    <div className={css.eventItem}>
+      <div className={css.imageThumb}>
         <div className={css.badges}>
           <div className={css.badge}>
             <span>{event.category}</span>
@@ -33,13 +26,11 @@ export const EventCard = ({ event }) => {
         </div>
         <p className={css.descriptionTitle}>{event.title}</p>
         <span className={css.descriptionText}>{event.description}</span>
-      </div>
 
-      {showMore && (
         <Link to={`/${event.id}`} className={css.moreButton}>
           Learn more
         </Link>
-      )}
+      </div>
     </div>
   );
 };
