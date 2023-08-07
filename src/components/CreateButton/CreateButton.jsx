4240aca47a5ecb2ebcfc6d7c7bd8800f5be63a07
Artem Karpenko <dante120591@gmail.com>
2023-08-07 23:34:51 +0300

    Hovers added

--- src/components/CreateButton/CreateButton.jsx
+++ src/components/CreateButton/CreateButton.jsx
@@ -6,7 +6,7 @@ import css from './CreateButton.module.css';
 
 export const CreateButton = () => {
   return (
-    <Link to="create" className={css.serviceButton}>
+    <Link to="create" className={css.buttonContainer}>
       <AiOutlinePlus style={{ width: 24, height: 24, fill: '#fff' }} />
       <span className={css.buttonName}>Add new event</span>
     </Link>
